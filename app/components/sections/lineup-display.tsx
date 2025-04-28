9b1a7b516050ed818fb297a4d9a3d2cb9e6b455a
Mads Nylund <madsnyl@gmail.com>
2025-04-28 15:34:16 +0200

    added persons and started on faq

--- app/components/sections/lineup-display.tsx
+++ app/components/sections/lineup-display.tsx
@@ -65,54 +65,122 @@ const members: {
         name: "Trym Gudvangen",
         avatar: "/image/trym.png",
         role: "Datateknologi",
+    },
+    {
+        name: "Caroline Floen",
+        avatar: "/image/carro.png",
+        role: "Marin teknikk"
+    },
+    {
+        name: "Benedikte Meyer",
+        avatar: "/image/benedikte.png",
+        role: "Økonomi og administrasjon"
+    },
+    {
+        name: "Kasper Sandø",
+        avatar: "/image/kasper.png",
+        role: "Digital Forretningsutvikling"
+    },
+    {
+        name: "Erle Røkke",
+        avatar: "/image/erle.png",
+        role: "Digital Forretningsutvikling"
     }
-]
+];
+
+const teamMembers: {
+    name: string;
+    avatar: string;
+    role: string;
+    link?: string;
+}[] = [
+    {
+        name: "Jonas Røisland",
+        avatar: "/image/jonas.png",
+        role: "MKI",
+    }
+];
 
 export default function LineupSection() {
     return (
-        <section className="bg-neutral-900 border-y border-y-neutral-800 py-16 md:py-32 mt-24">
-            <div className="mx-auto max-w-6xl border-t px-6">
-                <span className="text-caption -ml-6 -mt-3.5 block w-max px-6 bg-neutral-900">Lineup</span>
-                <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
-                    <div className="sm:w-2/5">
-                        <h2 className="text-3xl font-bold sm:text-4xl">Årets Deltagere</h2>
+        <section className="bg-neutral-900 border-y border-y-neutral-800 py-16 md:py-32 mt-24 space-y-24">
+                <div className="mx-auto max-w-6xl border-t px-6">
+                    <span className="text-caption -ml-6 -mt-3.5 block w-max px-6 bg-neutral-900">Lineup</span>
+                    <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
+                        <div className="sm:w-2/5">
+                            <h2 className="text-3xl font-bold sm:text-4xl">Årets Deltagere</h2>
+                        </div>
+                        <div className="mt-6 sm:mt-0 space-y-2">
+                            <h1 className="text-2xl font-medium">
+                                Ønsker du å delta?
+                            </h1>
+                            <p>
+                                Ta kontakt med Mads Nylund for mer informasjon.
+                            </p>
+                        </div>
                     </div>
-                    <div className="mt-6 sm:mt-0 space-y-2">
-                        <h1 className="text-2xl font-medium">
-                            Ønsker du å delta?
-                        </h1>
-                        <p>
-                            Ta kontakt med Mads Nylund for mer informasjon.
-                        </p>
-                    </div>
-                </div>
-                <div className="mt-12 md:mt-24">
-                    <div className="grid gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
-                        {members.map((member, index) => (
-                            <div key={index} className="group overflow-hidden">
-                                <img className="h-96 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem] group-hover:rounded-xl" src={member.avatar} alt="team member" width="826" height="1239" />
-                                <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
-                                    <div className="flex justify-between">
-                                        <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wider">{member.name}</h3>
-                                        <span className="text-xs">_0{index + 1}</span>
-                                    </div>
-                                    <div className="mt-1 flex items-center justify-between">
-                                        <span className="text-muted-foreground inline-block translate-y-6 text-sm opacity-0 transition duration-300 group-hover:translate-y-0 group-hover:opacity-100">{member.role}</span>
-                                        {member.link && (
-                                            <Link
-                                                to={member.link} 
-                                                className=" group-hover:text-primary-600 group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100"
-                                            >
-                                                Se mer
-                                            </Link>
-                                        )}
+                    <div className="mt-12 md:mt-24">
+                        <div className="grid gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+                            {members.map((member, index) => (
+                                <div key={index} className="group overflow-hidden">
+                                    <img className="h-96 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem] group-hover:rounded-xl" src={member.avatar} alt="team member" width="826" height="1239" />
+                                    <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
+                                        <div className="flex justify-between">
+                                            <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wider">{member.name}</h3>
+                                            <span className="text-xs">_0{index + 1}</span>
+                                        </div>
+                                        <div className="mt-1 flex items-center justify-between">
+                                            <span className="text-muted-foreground inline-block translate-y-6 text-sm opacity-0 transition duration-300 group-hover:translate-y-0 group-hover:opacity-100">{member.role}</span>
+                                            {member.link && (
+                                                <Link
+                                                    to={member.link} 
+                                                    className=" group-hover:text-primary-600 group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100"
+                                                >
+                                                    Se mer
+                                                </Link>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                <div className="mx-auto max-w-6xl border-t px-6">
+                    <span className="text-caption -ml-6 -mt-3.5 block w-max px-6 bg-neutral-900">Team</span>
+                    <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
+                        <div className="sm:w-2/5">
+                            <h2 className="text-3xl font-bold sm:text-4xl">Årets Dommere</h2>
+                        </div>
+                    </div>
+                    <div className="mt-12 md:mt-24">
+                        <div className="grid gap-x-6 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+                            {teamMembers.map((member, index) => (
+                                <div key={index} className="group overflow-hidden">
+                                    <img className="h-96 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem] group-hover:rounded-xl" src={member.avatar} alt="team member" width="826" height="1239" />
+                                    <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
+                                        <div className="flex justify-between">
+                                            <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wider">{member.name}</h3>
+                                            <span className="text-xs">_0{index + 1}</span>
+                                        </div>
+                                        <div className="mt-1 flex items-center justify-between">
+                                            <span className="text-muted-foreground inline-block translate-y-6 text-sm opacity-0 transition duration-300 group-hover:translate-y-0 group-hover:opacity-100">{member.role}</span>
+                                            {member.link && (
+                                                <Link
+                                                    to={member.link} 
+                                                    className=" group-hover:text-primary-600 group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100"
+                                                >
+                                                    Se mer
+                                                </Link>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
         </section>
     )
 }
--- app/routes/faq.tsx
+++ app/routes/faq.tsx
@@ -1,30 +1,85 @@
 import { HeroHeader } from '@/components/blocks/header'
+import FooterSection from '@/components/sections/footer'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion';
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/faq')({
   component: RouteComponent,
 })
 
+const faqItems: {
+  id: string;
+  question: string;
+  answer: string;
+}[] = [
+  {
+      id: 'item-1',
+      question: 'Hvor langt skal man løpe?',
+      answer: 'Løpet er totalt 1,6 km langt, som tilsvarer 4 runder rundt idrettsbanen. Hver runde er 400 meter.',
+  },
+  {
+      id: 'item-2',
+      question: 'Er det noen spesifikke krav til ølen?',
+      answer: 'Ja, hver deltager må bruke 4 flasker med 0,33 liter Dahls øl. Ingen annen drikke er tillatt.',
+  },
+  {
+      id: 'item-3',
+      question: 'Hva skjer hvis jeg spyr?',
+      answer: 'Hvis du spyr, må du ta en strafferunde. En strafferunde er en runde på 400 meter. Hvis du spyr på banen, blir du diskvalifisert fra løpet.',
+  },
+  {
+      id: 'item-4',
+      question: 'Kan jeg få refusjon hvis jeg ikke er fornøyd med Foodora leveransen?',
+      answer: 'Jeg vet ikke hvorfor dere fortsetter å spørre om dette. Det er ikke mulig å få refusjon for en Foodora leveranse. Hvis du har problemer med en bestilling, vennligst kontakt Foodora kundeservice direkte.',
+  },
+  {
+      id: 'item-5',
+      question: 'Hvordan lager man en bocadillo?',
+      answer: 'En bocadillo er en spansk sandwich laget med crusty brød og fylt med en liste spesifikke ingredienser. Du fyller bocadilloen med smør, stekte tomater, serranoskinke og brie. Hvis du hører noen andre si noe annet, så er det feil. Det er ingen andre måter å lage en bocadillo på.',
+  },
+];
+
 function RouteComponent() {
   return (
     <>
       <HeroHeader />
       <main className="overflow-hidden bg-background text-white min-h-screen px-4">
-        <div
-          aria-hidden
-          className="absolute inset-0 isolate opacity-65 contain-strict lg:block">
-          <div className="w-140 h-320 -translate-y-87.5 absolute left-0 top-0 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
-          <div className="h-320 absolute left-0 top-0 w-60 -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)] [translate:5%_-50%]" />
-          <div className="h-320 -translate-y-87.5 absolute left-0 top-0 w-60 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
-        </div>
+      <section className="py-16 md:py-24">
+            <div className="mx-auto max-w-5xl px-4 md:px-6">
+                <div className="mx-auto max-w-xl text-center">
+                    <h2 className="text-balance text-3xl font-bold md:text-4xl lg:text-5xl">Ofte stilte spørsmål</h2>
+                    <p className="text-muted-foreground mt-4 text-balance">
+                      Her finner du svar på de vanligste spørsmålene våre. Hvis du ikke finner det du leter etter, er du velkommen til å kontakte oss for ytterligere hjelp.
+                    </p>
+                </div>
 
-        <div className="space-y-12 pb-12 pt-28 max-w-4xl w-full mx-auto">
-          <h1 className="text-4xl font-bold text-center">Ofte stilte spørsmål</h1>
-          <p className="text-center">
-            Kommer snart
-          </p>
-        </div>
+                <div className="mx-auto mt-12 max-w-xl">
+                    <Accordion
+                        type="single"
+                        collapsible
+                        className="bg-muted dark:bg-muted/50 w-full rounded-2xl p-1">
+                        {faqItems.map((item) => (
+                            <div
+                                className="group"
+                                key={item.id}>
+                                <AccordionItem
+                                    value={item.id}
+                                    className="data-[state=open]:bg-card dark:data-[state=open]:bg-muted peer rounded-xl border-none px-7 py-1 data-[state=open]:border-none data-[state=open]:shadow-sm">
+                                    <AccordionTrigger className="cursor-pointer text-base hover:no-underline">{item.question}</AccordionTrigger>
+                                    <AccordionContent>
+                                        <p className="text-base">{item.answer}</p>
+                                    </AccordionContent>
+                                </AccordionItem>
+                                <hr className="mx-7 border-dashed group-last:hidden peer-data-[state=open]:opacity-0" />
+                            </div>
+                        ))}
+                    </Accordion>
+                </div>
+            </div>
+        </section>
       </main>
+
+      <FooterSection />
     </>
   )
 }
